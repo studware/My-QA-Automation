824798363f5ac273decba604bb8cc7e004b12505
eljunia <angela.teneva@gmail.com>
2019-03-11 20:46:32 +0200

    GoogleSearch fixed.

--- SeleniumBasics/SeleniumBasics/GoogleSearch.cs
+++ SeleniumBasics/SeleniumBasics/GoogleSearch.cs
@@ -2,6 +2,7 @@
 {
     using NUnit.Framework;
     using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
     using System.Threading;
 
     [TestFixture]
@@ -30,26 +31,25 @@
             Assert.AreEqual("Selenium", searchValue);
 
             //Arrange&Act
-            driver.FindElement(By.TagName("center")).Click();
+            driver.FindElement(By.TagName("center")).FindElement(By.Name("btnK")).Click();
+            IWebElement seleniumLink = wait.Until<IWebElement>(fl => { 
+                                        return fl.FindElement(By.Id("rso"))
+                                        .FindElement(By.TagName("a")); });
 
             Thread.Sleep(2000);
 
-            IWebElement firstLink = wait.Until<IWebElement>(fl => 
-                            { return fl.FindElement(
-                            By.XPath("//*[@id='rso']/div/div/div[1]/div/div/div[1]/a[1]")); });
-
-            string firstLinkText = firstLink.Text;
-            firstLink.Click();
-
-            //Assert
-            Assert.That(firstLinkText.Contains("Selenium - Web Browser Automation"));
+           string seleniumLinkText = seleniumLink.Text;
+           seleniumLink.Click();
+           
+           //Assert
+           Assert.That(seleniumLinkText.Contains("Selenium - Web Browser Automation"));
             string url = driver.Url;
             Assert.IsNotNull(url);
             Assert.AreEqual("https://www.seleniumhq.org/", url);
 
             //Arrange
-            IWebElement title = wait.Until<IWebElement>(d => { return d.FindElement(By.XPath("/html/head/title"));});
-            string pageTitle = title.Text.Trim();
+            IWebElement title = wait.Until<IWebElement>(d => { return d.FindElement(By.TagName("title"));});
+            string pageTitle = title.GetAttribute("text").Trim();
 
             //Assert
             Assert.IsNotNull(pageTitle);
@@ -59,3 +59,15 @@
         }
     }
 }
+
+/*      Using XPath for Selenium URL without wait:
+     
+        IWebElement firstLink = driver.FindElement(By
+            .XPath(@"//*[@id=""rso""]/div[1]/div/div/div[2]/div[1]/a")); */
+
+/*      Using XPath for Selenium URL with wait:
+      
+    IWebElement seleniumLink = wait.Until<IWebElement>(fl => {
+        return fl.FindElement(
+        By.XPath("//*[@id='rso']/div/div/div[1]/div/div/div[1]/a[1]"));});      */
+
